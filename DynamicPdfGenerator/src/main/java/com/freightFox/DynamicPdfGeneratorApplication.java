07d3e0ae283e6cb305f1d8af86645279db118864
suresh170411 <suresh.20fk@gmail.com>
2023-04-19 08:02:54 +0530

    Created project with all the dependencies

---
+++ DynamicPdfGenerator/src/main/java/com/freightFox/DynamicPdfGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.freightFox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicPdfGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicPdfGeneratorApplication.class, args);
+	}
+
+}
