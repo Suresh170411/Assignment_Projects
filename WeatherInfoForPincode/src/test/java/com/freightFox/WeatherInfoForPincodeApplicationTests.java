6698e482b204a2a4077c62b7112d51ca5436d22f
suresh170411 <suresh.20fk@gmail.com>
2023-04-20 11:38:46 +0530

    INTIAL : added new project template having all the dependency and packages

---
+++ WeatherInfoForPincode/src/test/java/com/freightFox/WeatherInfoForPincodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freightFox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherInfoForPincodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
